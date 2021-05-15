48e5f9652c9174cb727c5a3627e961536bae4572
PetraR <peta.rebrosova@gmail.com>
2021-05-15 23:11:22 +0200

    Remove unused constants

--- myApp/app/src/main/java/com/example/myapplication/utils/Constants.kt
+++ myApp/app/src/main/java/com/example/myapplication/utils/Constants.kt
@@ -3,16 +3,10 @@ package com.example.myapplication.utils
 class Constants {
     companion object {
         const val PROD_PROJECT_ID = "07af6ff3-0f28-0106-770b-39743a18ad22"
-        const val LOCAL_HTTP_PROJECT_ID = "faa3cb57-7b91-0128-87ca-b4647da33b36"
         const val LOCAL_GRPC_PROJECT_ID = "faa3cb57-7b91-0128-87ca-b4647da33b36"
-        const val GRPC_API_URL = "http://localhost:5000"
         const val LOCALHOST = "localhost"
-        const val PORT_NUMBER_GRPC = 5000
-        const val PORT_NUMBER_HTTP = 19710
-        const val IP = "192.168.0.122"
 
         // connection to the gRPC API from the emulator
         const val GRPC_LOCALHOST_EMULATOR = "10.0.2.2:5000"
-        const val HTTP_LOCALHOST_EMULATOR = "http://10.0.2.2:19710"
     }
 }
